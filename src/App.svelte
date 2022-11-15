82f58181e5f5b2b5b8556d08265b294db04a9667
Heikki Ketoharju <heikki.ketoharju@iki.fi>
2022-11-15 22:53:14 +0200

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main>
+    <h1>Hello, world!</h1>
+    <Counter></Counter>
+</main>
+
+<style></style>
